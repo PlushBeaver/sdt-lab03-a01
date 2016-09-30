5ac35495898c5f45c10d606b57592d968b264696
Dmitry Kozliuk <dmitry.kozliuk@gmail.com>
2016-09-30 11:30:36 +0300

    код разбора

--- parser.cc
+++ parser.cc
@@ -19,27 +19,57 @@ parse_scheme(istream& input) {
     scheme.first = nullptr;
     scheme.last = nullptr;
 
-    // TODO: считать первое слово.
-    // TODO: ока последнее считанное слово — "line", разобрать описание линии.
+    // Считать первое слово.
+    string keyword = read_word(input);
+
+    // Пока последнее считанное слово — "line", разобрать описание линии.
+    while (keyword == "line") {
+        parse_line(input, keyword, scheme);
+    }
 
     return scheme;
 }
 
 void
 parse_line(std::istream& input, std::string& keyword, Scheme& scheme) {
-    // TODO: считать название линии и находить либо создавать её.
-    // TODO: считать очередное слово и циклически разбирать описание станции.
+    string name = read_word(input);
+    Line* line = get_line(scheme, name);
+
+    keyword = read_word(input);
+    while (keyword == "station") {
+        parse_station(input, keyword, scheme, line);
+    }
 }
 
 void
 parse_station(istream& input, string& keyword, Lines& scheme, Line* line) {
-    // TODO: считать название линии и либо создавать её, либо перемещать в конец линии.
-    // TODO: считать очередное слово и циклически разбирать описание перехода.
+    string name = read_word(input);
+    Station* station = find_station(*line, name);
+    if (station != nullptr) {
+        move_to_end(*line, station);
+    } else {
+        station = add_station(*line, name);
+    }
+
+    keyword = read_word(input);
+
+    while (keyword == "transit") {
+        parse_transit(input, keyword, scheme, station);
+    }
 }
 
 void
 parse_transit(istream& input, string& keyword, Scheme& scheme, Station* station) {
-    // TODO: считать название станции и линии назначения, найти или создать их,
-    //       добавить переход.
-    // TODO: считать очередное слово.
+    read_word(input);  // to
+    read_word(input);  // station
+    string station_name = read_word(input);
+    read_word(input);  // of
+    string line_name = read_word(input);
+    read_word(input);  // line
+
+    Line* sibling_line = get_line(scheme, line_name);
+    Station* sibling_station = get_station(*sibling_line, station_name);
+    add_transit(station, sibling_station);
+
+    keyword = read_word(input);
 }
