4128eadd203caeb9f3d82bf94a9f632d52eecfa1
Daniil Protasov <pds2014>
2016-10-28 10:59:10 +0400

    protasov

--- output.cc
+++ output.cc
@@ -18,7 +18,7 @@ operator<<(ostream& output, const Station& station) {
     Transit* current = station.transits.first;
     while (current)
     {
-      output << *current <<endl;
+      output <<"Transit to station "<< current->destination->name<<endl;
       current=current->next;
     }
     return output;
@@ -26,12 +26,50 @@ operator<<(ostream& output, const Station& station) {
 
 ostream&
 operator<<(ostream& output, const Line& line) {
+    output << "line "<< line.name<<"\"\n";
+    Station* current = line.stations.first;
+    while (current)
+    {
+      output <<"Station "<< current->name <<endl;
+      if (current->transits.first != nullptr)
+      {
+      Transit* tekus = current->transits.first;
+       while (tekus)
+       {
+       output <<"transit to station "<<tekus->destination->name <<endl;
+       tekus= tekus->next;
+       }
+      }
+    current=current->next;
+    }
     // TODO: печатать линию.
     return output;
 }
 
 ostream&
 operator<<(ostream& output, const Scheme& scheme) {
+    Line* current = scheme.first;
+    output<<"scheme metro "<<endl;
+    while (current)
+    {
+     output<<"Line "<< current->name <<endl;
+      Station* tekus = current->stations.first;
+      while (tekus)
+    {
+      output <<"Station "<< tekus->name <<endl;
+      if (tekus->transits.first != nullptr)
+      {
+      Transit* tekus1 = tekus->transits.first;
+       while (tekus1)
+       {
+       output <<"transit to station "<<tekus1->destination->name<<endl;
+       tekus1= tekus1->next;
+       }
+      }
+      tekus=tekus->next;
+    }
+    current=current->next;
+    }
     // TODO: печатать схему.
     return output;
 }
